22226c8f4942837aafded1ab1f33613824f8f727
snith nhotprasa <lesnith@gmail.com>
2024-06-24 18:02:01 +0200

    image depuis cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,29 +24,146 @@ users.each do |user_data|
   User.create!(user_data)
 end
 
+require 'open-uri'
+
 puts 'Creating Heros...'
 heros = [
-  { name: "Tchokolatine", role: "support", picture: "app/assets/images/hero_images/tchokolatine.jpeg", job: "cleric", guild: "Trinity", description: "Premium healer, won't run out of mana. You'll feel invincible!", price: 999, user: User.find_by(email: "user1@hero2go.com") },
-  { name: "Victor G. mane", role: "tank", picture: "app/assets/images/hero_images/victor_g_mane.jpg", job: "knight", guild: "Trinity", description: "With a full gold armor. Pricy and arrogant but the most efficient human shield!", price: 9999, user: User.find_by(email: "user1@hero2go.com") },
-  { name: "Kayno", role: "magical", picture: "app/assets/images/hero_images/kayno.jpg", job: "wizard", guild: "Trinity", description: "Smart and strong...will BURN all your enemies in a blink of an eye...", price: 500, user: User.find_by(email: "user1@hero2go.com") },
-  { name: "Kofi", role: "physical", picture: "app/assets/images/hero_images/kofi.jpg", job: "fighter", guild: "Le Wagon", description: "He won't sleep in the battlefield", price: 500,user: User.find_by(email: "user2@hero2go.com") },
-  { name: "Tee", role: "support", picture: "app/assets/images/hero_images/tee.jpg", job: "alchemist", guild: "Le Wagon", description: "Can give some extra juice to your fighters. Come with extra secret cocktail for free!", price: 800, user: User.find_by(email: "user2@hero2go.com") },
-  { name: "Mug", role: "physical", picture: "app/assets/images/hero_images/mug.jpg", job: "ranger", guild: "Le Wagon", description: "Your opponent will only be able to hear the knock of deathwhistle", price: 890, user: User.find_by(email: "user2@hero2go.com") },
-  { name: "Sol B.", role: "tank", picture: "app/assets/images/hero_images/sol_b.jpg", job: "paladin", guild: "The Fabulous", description: "A.K.A. Order Sol. The real bad guy", price: 400, user: User.find_by(email: "user3@hero2go.com") },
-  { name: "Dan'Ghul", role: "magical", picture: "app/assets/images/hero_images/dan_ghul.jpg", job: "warlock", guild: "The Fabulous", description: "So powerful that some other warlock litteraly copy his name", price: 1500, user: User.find_by(email: "user3@hero2go.com") },
-  { name: "Benedict Strange", role: "magical", picture: "app/assets/images/hero_images/benedict_strange.jpg", job: "wizard", guild: "The Fabulous", description: "The most briliant time bender", price: 1000, user: User.find_by(email: "user3@hero2go.com") },
-  { name: "Maron Bordo", role: "magical", picture: "app/assets/images/hero_images/maron_bordo.jpg", job: "warlock", guild: "Haze Roth", description: "The famous mana leecher", price: 2000, user: User.find_by(email: "user4@hero2go.com") },
-  { name: "Anne Dwin", role: "support", picture: "app/assets/images/hero_images/anne_dwin.jpg", job: "cleric", guild: "Haze Roth", description: "Daughter of queen Varyan, from the most famous cleric order", price: 2000, user: User.find_by(email: "user4@hero2go.com") },
-  { name: "The Hollow", role: "tank", picture: "app/assets/images/hero_images/the_hollow.jpg", job: "knight", guild: "Haze Roth", description: "with a strange insect shaped armor,The Hollow will deflect all the attacks", price: 2200, user: User.find_by(email: "user4@hero2go.com") },
-  { name: "Dee Luffy", role: "support", picture: "app/assets/images/hero_images/dee_luffy.jpg", job: "monk", guild: "Omnislash", description: "The famous monkey, ex-pirate, now can heal you with mixture of strange recipes and prayers", price: 10, user: User.find_by(email: "user5@hero2go.com") },
-  { name: "Malefixx", role: "physical", picture: "app/assets/images/hero_images/malefixx.jpg", job: "assassin", guild: "Omnislash", description: "....Slash....", price: 15, user: User.find_by(email: "user5@hero2go.com") },
-  { name: "Gougeul Stryfe", role: "tank", picture: "app/assets/images/hero_images/gougeul_stryfe.jpg", job: "knight", guild: "Omnislash", description: "The best soldier from materia academy", price: 50, user: User.find_by(email: "user5@hero2go.com") }
+  { name: "Tchokolatine",
+    role: "support",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241478/development/tchokolatine_benpjv.jpg",
+    job: "cleric", guild: "Trinity", description: "Premium healer, won't run out of mana. You'll feel invincible!",
+    price: 999, user: User.find_by(email: "user1@hero2go.com")},
+
+  { name: "Victor G. mane",
+    role: "tank",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241485/development/victor_g_mane_imh9x8.jpg",
+    job: "knight", guild: "Trinity",
+    description: "With a full gold armor. Pricy and arrogant but the most efficient human shield!",
+    price: 9999, user: User.find_by(email: "user1@hero2go.com")},
+
+  { name: "Kayno",
+    role: "magical",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241444/development/kayno_v17ttc.jpg",
+    job: "wizard",
+    guild: "Trinity",
+    description: "Smart and strong...will BURN all your enemies in a blink of an eye...",
+    price: 500,
+    user: User.find_by(email: "user1@hero2go.com") },
+
+  { name: "Kofi",
+    role: "physical",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241469/development/kofi_kriuso.jpg",
+    job: "fighter", guild: "Le Wagon",
+    description: "He won't sleep in the battlefield",
+    price: 500,
+    user: User.find_by(email: "user2@hero2go.com") },
+
+  { name: "Tee",
+    role: "support",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241480/development/tee_npwvwn.jpg",
+    job: "alchemist",
+    guild: "Le Wagon",
+    description: "Can give some extra juice to your fighters. Come with extra secret cocktail for free!",
+    price: 800,
+    user: User.find_by(email: "user2@hero2go.com") },
+
+  { name: "Mug",
+    role: "physical",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241475/development/mug_e44f13.jpg",
+    job: "ranger",
+    guild: "Le Wagon",
+    description: "Your opponent will only be able to hear the knock of deathwhistle",
+    price: 890,
+    user: User.find_by(email: "user2@hero2go.com") },
+
+  { name: "Sol B.",
+    role: "tank",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241477/development/sol_b_wrlvhg.jpg",
+    job: "paladin",
+    guild: "The Fabulous",
+    description: "A.K.A. Order Sol. The real bad guy",
+    price: 400,
+    user: User.find_by(email: "user3@hero2go.com") },
+
+  { name: "Dan'Ghul",
+    role: "magical",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241415/development/dan_ghul_bnscff.jpg",
+    job: "warlock",
+    guild: "The Fabulous",
+    description: "So powerful that some other warlock litteraly copy his name",
+    price: 1500,
+    user: User.find_by(email: "user3@hero2go.com") },
+
+  { name: "Benedict Strange",
+    role: "magical",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241413/development/benedict_strange_nurnfx.jpg",
+    job: "wizard",
+    guild: "The Fabulous",
+    description: "The most briliant time bender",
+    price: 1000,
+    user: User.find_by(email: "user3@hero2go.com") },
+
+  { name: "Maron Bordo",
+    role: "magical",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241473/development/maron_bordo_pwmwnf.jpg",
+    job: "warlock",
+    guild: "Haze Roth",
+    description: "The famous mana leecher",
+    price: 2000,
+    user: User.find_by(email: "user4@hero2go.com") },
+
+  { name: "Anne Dwin",
+    role: "support",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241412/development/anne_dwin_ypu5v4.jpg",
+    job: "cleric",
+    guild: "Haze Roth",
+    description: "Daughter of queen Varyan, from the most famous cleric order",
+    price: 2000,
+    user: User.find_by(email: "user4@hero2go.com") },
+
+  { name: "The Hollow",
+    role: "tank",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241482/development/the_hollow_cho308.jpg",
+    job: "knight",
+    guild: "Haze Roth",
+    description: "with a strange insect shaped armor,The Hollow will deflect all the attacks",
+    price: 2200,
+    user: User.find_by(email: "user4@hero2go.com") },
+
+  { name: "Dee Luffy",
+    role: "support",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241417/development/dee_luffy_oi5tol.jpg",
+    job: "monk",
+    guild: "Omnislash",
+    description: "The famous monkey, ex-pirate, now can heal you with mixture of strange recipes and prayers",
+    price: 10,
+    user: User.find_by(email: "user5@hero2go.com") },
+
+  { name: "Malefixx",
+    role: "physical",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241471/development/malefixx_bxuyyj.jpg",
+    job: "assassin",
+    guild: "Omnislash",
+    description: "....Slash....",
+    price: 15,
+    user: User.find_by(email: "user5@hero2go.com") },
+
+  { name: "Gougeul Stryfe",
+    role: "tank",
+    picture: "https://res.cloudinary.com/dlh0ap8um/image/upload/v1719241442/development/gougeul_stryfe_gtiwdr.jpg",
+    job: "knight",
+    guild: "Omnislash",
+    description: "The best soldier from materia academy",
+    price: 50,
+    user: User.find_by(email: "user5@hero2go.com") }
 ]
 
 heros.each do |hero_data|
   picture = hero_data.delete(:picture)
   hero = Hero.new(hero_data)
-  hero.picture.attach(io: File.open(picture), filename: picture, content_type: "image/jpg")
+  file = URI.open(picture)
+
+  hero.picture.attach(io: file, filename: File.basename(picture), content_type: "image/jpg")
   hero.save
 end
 
