07ebfaeca93e0ed39e430ada355a9ad41f58afcb
snith nhotprasa <lesnith@gmail.com>
2025-03-28 15:17:38 +0100

    update de flatpickr mais dysfonctions

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,7 +3,14 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static targets = ["startDate", "endDate"];
   connect() {
-    flatpickr(this.element)
+    console.log("Connected to datepicker controller");
+    flatpickr(this.startDateTarget, {
+    
+    });
+    flatpickr(this.endDateTarget, {
+
+    });
   }
 }
